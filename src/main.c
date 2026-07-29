abdf0c855abd99231a3da347b5875164624c21fa
matthewlefler <matthewlefler1@gmail.com>
2026-07-29 19:27:03 -0400

    init copy paste from treeWalk prototyping

---
+++ src/main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <vulkan/vk_enum_string_helper.h>
+#include <vulkan/vulkan.h>
+
+#include "utilities/logger/logger.h"
+
+#include "vulkan_renderer/render.h"
+#include "vulkan_renderer/log_utilities.h"
+
+uint32_t score_physical_device(VkPhysicalDeviceProperties2 device_properties_2) {
+    return 1 + \
+        (device_properties_2.properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) * 0b100;
+}
+
+int main(int argc, char** argv)
+{
+    set_log_level(LOG_LEVEL_VERBOSE);
+
+    VkResult result;
+
+    // requirements
+        VkPhysicalDeviceDynamicRenderingFeatures dynamic_rendering = {
+            .dynamicRendering = VK_TRUE,
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_DYNAMIC_RENDERING_FEATURES,
+        };
+
+        VkPhysicalDeviceVulkan11Features vulkan_11 = {
+            .shaderDrawParameters = VK_TRUE,
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_FEATURES,
+        };
+
+        VkPhysicalDeviceExtendedDynamicStateFeaturesEXT extended_dynamic_state = {
+            .extendedDynamicState = VK_TRUE,
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_EXTENDED_DYNAMIC_STATE_FEATURES_EXT,
+        };
+
+        VkPhysicalDeviceFeatures2 device_features_2 = {
+            .features.geometryShader = VK_TRUE,
+            .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2,
+        };
+        
+        // chain of features to grab
+        device_features_2.pNext = &dynamic_rendering;
+        dynamic_rendering.pNext = &vulkan_11;
+        vulkan_11.pNext = &extended_dynamic_state;
+        extended_dynamic_state.pNext = NULL;
+
+        VkQueueFlagBits flagbits = VK_QUEUE_GRAPHICS_BIT;
+
+    Renderer renderer;
+    result = vulkan_renderer_init(&renderer, &score_physical_device, &device_features_2, 1, &flagbits);
+    log_message(LOG_LEVEL_DEBUG, "init result: %s", string_VkResult(result));
+
+    result = vulkan_renderer_run(&renderer);
+    log_message(LOG_LEVEL_DEBUG, "run result: %s", string_VkResult(result));
+
+    result = vulkan_renderer_cleanup(&renderer);
+    log_message(LOG_LEVEL_DEBUG, "cleanup result: %s", string_VkResult(result));
+
+    return EXIT_SUCCESS;
+}
---
+++ src/utilities/logger/logger.c
@@ -0,0 +1,77 @@
+#ifndef LOGGER_C
+#define LOGGER_C
+
+#include <stdio.h>
+#include <time.h>
+#include <stdatomic.h>
+#include <stdarg.h>
+
+#include "logger.h"
+
+_Atomic unsigned char CURRENT_LOG_LEVEL;
+
+#ifdef NDEBUG
+
+inline void set_log_level(LOG_LEVEL level) {
+    return
+}
+
+inline void log_message(LOG_LEVEL level, const char* message, ...) {
+    return
+}
+
+#else
+
+void set_log_level(LOG_LEVEL level) {
+    atomic_store(&CURRENT_LOG_LEVEL, (_Atomic unsigned char) level);
+    log_message(LOG_LEVEL_INFO, "set log level to %s", log_level_to_string(level));
+}
+
+void log_message(LOG_LEVEL level, const char* message, ...) {
+    // get current log level
+    LOG_LEVEL current_log_level = (LOG_LEVEL) atomic_load(&CURRENT_LOG_LEVEL);
+
+    if(level < current_log_level) {
+        return;
+    }
+
+    // get time
+    time_t rawtime = time(NULL);
+    char* time_string = ctime(&rawtime);
+    // replace newlines with null terminators
+    for(char* c = time_string; *c != '\0'; ++c) {
+        if(*c == '\n' || *c == '\r') {
+            *c = '\0';
+        }
+    }
+
+    // get log_level equvilent string
+    const char const* log_level_string = log_level_to_string(level);
+
+    // print header
+    printf("[%s] %s: ", time_string, log_level_string);
+    // print message
+    va_list args;
+    va_start(args, message);
+    vprintf(message, args);
+    va_end(args);
+
+    printf("\n");
+}
+
+#endif
+
+const char const* log_level_to_string(LOG_LEVEL level) {
+    switch (level) {
+        case LOG_LEVEL_VERBOSE: return LOG_LEVEL_VERBOSE_STRING;
+        case LOG_LEVEL_DEBUG:   return LOG_LEVEL_DEBUG_STRING;
+        case LOG_LEVEL_INFO:    return LOG_LEVEL_INFO_STRING;
+        case LOG_LEVEL_WARNING: return LOG_LEVEL_WARNING_STRING;
+        case LOG_LEVEL_ERROR:   return LOG_LEVEL_ERROR_STRING;
+        case LOG_LEVEL_FATAL:   return LOG_LEVEL_FATAL_STRING;
+    
+        default: return LOG_LEVEL_UNKNOWN_STRING;
+    }
+}
+
+#endif
